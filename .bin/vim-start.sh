79407e43e38c3aba5b76b715f1de2f9410c874ac
perry <perry.hargrave@gmail.com>
2009-01-19 07:56:40 -0800

    works now opening or sending to vim server

--- .bin/vim-start.sh
+++ .bin/vim-start.sh
@@ -3,9 +3,24 @@
 #
 # to start vim with its own colors
 cmd="$(which vim)"
-if [ "$#" -gt "0" ] ; then
-  cmd="$cmd vim -s $@"
-fi
 xrdb -load ~/.Xdefaults.light
+
+if [ "$#" -gt "0" ] ; then
+  files="$@"
+fi
+
+srv="$($cmd --serverlist | head -1)"
+
+echo $srv
+
+# no existing vim server found use default master
+if [ -z "$srv" ] ; then 
+  srv="master" 
+  cmd="$cmd --servername $srv $files"
+else
+  cmd="$cmd --servername $srv --remote-silent $files"
+fi
+
 urxvtc -name vim -e sh -c "$cmd" &
+
 xrdb -load ~/.Xdefaults
