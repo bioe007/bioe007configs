55e19c44c0ba8d2756bf269fe77aba2686b85b45
perry <perry.hargrave@gmail.com>
2009-06-23 08:53:11 -0700

    fx for lvds, fix soyo resolution, remove wp setting

--- .bin/stupid.sh
+++ .bin/stupid.sh
@@ -13,20 +13,55 @@
 # echo $PREFMODE
 # exit 0
 
+
+# {{{ lvds 
+lvds () {
+  # wait until display is valid
+  while [[ -z "$DISPLAY" ]] ; do
+    sleep 1
+    iloop="$((iloop + 1))"
+  done
+  # check for LVDS in native mode
+  if [ -z "$(xrandr | grep ^LVDS | grep -w 1280x800)" ]; then
+    sudo xrandr --output LVDS --mode 1280x800
+    echo 'Setting LVDS to 1280x800'
+    iloop="0"
+  fi
+}
+# }}}
+
+off () {
+  xrandr --output VGA --off
+}
+# {{{ syncmaster 
 syncmaster () {
-  sudo xrandr --output LVDS --mode 1280x800
-  sudo xrandr --newmode 1280x1024_59.00 106.97 1280 1360 1496 1712 1024 1025 1028 1059 -HSync +Vsync
-  sudo xrandr --addmode VGA 1280x1024_59.00
+  lvds
+  if [ -z "$(xrandr | grep 1280x1024_59 | grep -w "**$")" ] ; then
+    sudo xrandr --newmode 1280x1024_59.00 106.97 1280 1360 1496 1712 1024 1025 1028 1059 -HSync +Vsync
+    sudo xrandr --addmode VGA 1280x1024_59.00
+  fi
   sudo xrandr --output VGA --mode 1280x1024_59.00 --left-of LVDS
 }
+# }}}
 
+# {{{ soyo 
 soyo () {
-  sudo xrandr --output LVDS --mode 1280x800
-  sudo xrandr --newmode 1440x900_60.00  106.47  1440 1520 1672 1904  900 901 904 932  -HSync +Vsync
-  sudo xrandr --addmode VGA 1440x900_60.00
-  sudo xrandr --output VGA --mode  1440x900_60.00 --right-of LVDS
+  lvds
+  if [ -z "$(xrandr | grep 1440x900_60 | grep -w "**$")" ] ; then
+    # sudo xrandr --newmode 1440x900_60.00  106.47  1440 1520 1672 1904  900 901 904 932  -HSync +Vsync
+    sudo xrandr --newmode 1680x1050_68.00  168.71  1680 1792 1976 2272  1050 1051 1054 1092  -HSync +Vsync
+    sudo xrandr --addmode VGA  1680x1050_68.00
+  fi
+  sudo xrandr --output VGA --mode 1680x1050_68.00 --right-of LVDS
+}
+# }}}
+
+mgen() {
+  mline="$(gtf 1440 900 ${refresh} | grep Modeline | sed s/Modeline\ // | tr -d '"' )"
+
 }
   
+# {{{ setup 
 setup () {
   answer=""
   mline=""
@@ -36,7 +71,9 @@ setup () {
   refresh="59"
   while [[ ! "$answer" == "y" ]] ; do
 
-    mline="$(gtf 1440 900 ${refresh} | grep Modeline | sed s/Modeline\ // | tr -d '"' )"
+    mline="$(gtf 1680 1050 ${refresh} | grep Modeline | sed s/Modeline\ // | tr -d '"' )"
+    # mline="$(mgen $1 $2 ${refresh})"
+    echo $mline
 
     sudo xrandr --newmode $mline
     sudo xrandr --addmode VGA "$(echo $mline | cut -f 1 -d ' ')"
@@ -67,23 +104,22 @@ setup () {
   echo "exiting with refresh=$((refresh -1))"
   echo "modeline=$mline"
 }
+# }}}
 
 if [[ "$1" == "--setup" ]] ; then 
   setup
   exit 0
 elif [[ "$1" == "--sync" ]] ; then
   syncmaster
-  nitrogen --restore
   exit 0
 elif [[ "$1" == "--soyo" ]] ; then
   soyo
-  nitrogen --restore
   exit 0
+elif [[ "$1" == "--off" ]] ; then
+  off
+  exit 0 
 else
-  iloop="0"
-  while [[ -z "$DISPLAY" ]] ; do
-    sleep 1
-    iloop="$((iloop + 1))"
-  done
-  sudo xrandr --output LVDS --mode 1280x800
+  lvds
 fi
+
+# vim:set filetype=sh fdm=marker tabstop=2 shiftwidth=2 expandtab smarttab autoindent smartindent: --
