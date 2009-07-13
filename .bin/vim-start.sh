fb82e668bb2432fa292a1681722e2314a38ae91d
perry <perry.hargrave@gmail.com>
2009-07-13 11:42:39 -0700

    fix the opening new files in existing session

--- .bin/vim-start.sh
+++ .bin/vim-start.sh
@@ -5,20 +5,28 @@
 # to start vim with its own colors
 cmd="$(which vim)"
 
+F2OPEN=""
 if [ "$#" -gt "0" ] ; then
   files="$@"
+  F2OPEN="1"
 fi
 
 srv="$($cmd --serverlist | head -1)"
 
-echo $srv
-
 # no existing vim server found use default master
 if [ -z "$srv" ] ; then 
   srv="master" 
-  cmd="$cmd --servername $srv \"$files\""
+
+  # open with files from cli or not
+  if [ "$F2OPEN" ] ; then
+    cmd="$cmd --servername $srv \"$files\""
+  else
+    cmd="$cmd --servername $srv" 
+  fi
 else
+  echo "Opening files in server: $srv"
   cmd="$cmd --servername $srv --remote-silent \"$files\""
 fi
 
+# opens vim in urxvt
 urxvt -bg '#2e3436' +tr -name vim -e sh -c "$cmd" &
