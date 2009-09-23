6432233de3db4371370f4a28c08b3c658fc61e13
perry <perry.hargrave@gmail.com>
2009-09-23 09:11:51 -0700

    make option parsing smarter, integrate with awesome wm

--- .bin/vim-start.sh
+++ .bin/vim-start.sh
@@ -2,34 +2,79 @@
 #
 # auth: bioe007
 # 
-# to start vim with its own colors
+# start vim server master. pass any other files to this instance of vim if
+# already running
+#
+#
 cmd="$(which vim)"
 
-F2OPEN=""
+options=""
+files=""
+
+# store cli options
 if [ "$#" -gt "0" ] ; then
-  files="$@"
-  F2OPEN="1"
+    if [ ! -z "$(echo $@ | grep "\-\-")" ] ; then
+        while [ ! -z "$( echo "$1" | grep "^\-\-" )" ] ; do
+            options="$options $1"
+            shift
+        done
+        cmd="$cmd $options"
+    fi
 fi
+
+# assume anything left is a file, only add readable files to the list
+while [ "$#" -ge "1" ] ; do
+    if [ -r "$1" ] ; then
+        files="$files $1"
+    fi
+    shift
+done
 
 srv="$($cmd --serverlist | head -1)"
 
 # no existing vim server found use default master
 if [ -z "$srv" ] ; then 
-  srv="master" 
+    srv="master"
 
-  # open with files from cli or not
-  if [ "$F2OPEN" ] ; then
-    cmd="$cmd --servername $srv \"$files\""
-  else
-    cmd="$cmd --servername $srv" 
-  fi
+    # open with files from cli or not
+    # also ignore any cli options so as not to confuse things
+    if [ -z "$files" ] ; then 
+        cmd="$cmd --servername $srv" 
+    else
+        cmd="$cmd --servername $srv \"$files\""
+    fi
+
 else
-  echo "Opening files in server: $srv"
-  cmd="$cmd --servername $srv --remote-silent \"$files\""
+
+    # if the server is running and no files are being opened or options/commands
+    # passed, then just exit
+    if [ -z "$files" ] && [ -z "$options" ] ; then
+        echo "Server running, no file arguments, no cli options. vim-start exiting"
+        exit 0
+    fi
+
+    # otherwise concatenate the relevant vars to do something
+    if [ ! -z "$files" ] && [ -z "$options" ] ; then
+
+        # quikcly split the current buffer before adding the files
+        $cmd --servername $srv --remote-send ':sp<CR>'
+        cmd="$cmd -c :sp --servername $srv $options --remote-silent  $files"
+
+    elif [ -z "$files" ] && [ ! -z "$options" ] ; then
+        cmd="$cmd --servername $srv $options"
+    fi
+
+    echo "$cmd"
+    echo 'call vimpop'
+
+    echo 'tagPop("vim")' | awesome-client > /dev/null 2>&1
+    exec $cmd
+    exit $?
 fi
 
 # opens vim in urxvt
-urxvt -bg '#2e3436' +tr -name vim -e sh -c "$cmd" &
+# urxvt -bg '#2e3436' +tr -name vim -e sh -c "$cmd" &
+urxvt -bg '#2e3436' +tr -name vim -e zsh -c "$cmd" &
 
 
 
